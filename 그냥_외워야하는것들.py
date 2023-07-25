34145149d1dbeca1dcedabd84b62d2af5ad0223f
DESKTOP-O459G30\hanbee <hanbee1123@gmail.com>
2023-07-25 21:56:05 +0900

    tester

--- 그냥_외워야하는것들.py
+++ 그냥_외워야하는것들.py
@@ -1,7 +1,8 @@
-#1. Tree Traversal BFS
+#1. Tree Traversal BFS, DFS
 from collections import deque
+
 def bfs(root):
-    if root is None:
+    if root == None:
         return None
     visited = []
     q = deque(root)
@@ -12,11 +13,9 @@ def bfs(root):
             q.append(cur_node.left)
         if cur_node.right:
             q.append(cur_node.right)
-
     return visited
+    
 
-
-#2. Tree Traversal DFS (preorder, inorder, postorder)
 def dfs_preorder(root):
     if root == None:
         return
@@ -24,21 +23,49 @@ def dfs_preorder(root):
     dfs_preorder(root.left)
     dfs_preorder(root.right)
 
-def dfs_inorder(root):
-    if root is None:
-        return
-    dfs_inorder(root.left)
-    print(root.val)
-    dfs_inorder(root.right)
 
-def dfs_postorder(root):
-    if root is None:
-        return
-    dfs_postorder(root.left)
-    dfs_postorder(root.right)
-    print(root.val)
+#2. Graph Traversal
+def bfs_graph(graph,start):
+    visited = []
+    q = deque(start)
 
-#3. Graph Traversal BFS
+    while q:
+        cur_node = q.popleft()
+        visited.append(cur_node)
+        for j in graph[cur_node]:
+            if j not in visited:
+                q.append(j)
+    return visited
+
+def dfs_graph(graph,start,visited=[]):
+    visited.append(start)
+    for j in graph[start]:
+        if j not in visited:
+            dfs_graph(graph,j,visited)
+    return
+
+
+#3. dynamic programming bottomup, topdown
+
+def dp_bottomup(n):
+    values = {1:1, 2:1}
+    for i in range(3,n):
+        values[i] = values[i-1]+values[i-2]
+    return values[n]
+
+def dp_topdown(n):
+    memo = {}
+    def dp(n):
+        if n not in memo:
+            memo[n] = dp(n-1) + dp(n-2)
+        else:
+            memo[n]
+    
+    dp(n)
+    return memo[n]
+
+
+
     # graph = {
     #     'A':['B','D','E'],
     #     'B':['A','C','D'],
@@ -47,45 +74,6 @@ def dfs_postorder(root):
     #     'E':['A']
     # }
 
-def bfs(graph, start_point):
-    if graph is None:
-        return None
-    visited = [start_point]
-    q = deque(start_point)
-    while q:
-        for v in graph[start_point]:
-            if v not in visited:
-                visited.append(v)
-                q.append(v)
-    return visited
-
-def dfs(graph, start_point, visited=[]):
-    visited.append(start_point)
-    for v in graph[start_point]:
-        if v not in visited:
-            dfs(graph, v, visited)
-    return visited
-
-
-# dp topdown
-#dp bottom up
-
-def topdown(n):
-    memo = {}
-    def dp(n):
-        if n==0 or n==1: return 1
-        if n not in memo:
-            memo[n] = dp(n-1)+dp(n-2)
-        return memo[n]
-    dp(10)
-    return memo[n]
-
-def bottomup(n):
-    memo ={1:1,2:1}
-    for v in range(3,n):
-        memo[v] = memo[v-1] + memo[v-2]
-    
-    return memo[n]
 
 
 if __name__ == "__main__":
