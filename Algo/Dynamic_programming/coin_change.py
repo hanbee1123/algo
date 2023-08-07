fe8a768e173195a539f9cbbcce8c47b3f287e791
Han Bee Lee <46665763+hanbee1123@users.noreply.github.com>
2023-08-07 23:17:34 +0900

    dp_practice

--- Algo/Dynamic_programming/coin_change.py
+++ Algo/Dynamic_programming/coin_change.py
@@ -50,3 +50,20 @@ class Solution:
                     dp[i]=min(dp[i], dp[i-coin]+1)
         
         return -1 if dp[-1]==amount+1 else dp[-1]
+    
+def cc(coins,amount):
+    dp = [amount+1]*(amount+1)
+    dp[0] = 0
+
+    for a in range(1,amount+1):
+        for c in coins:
+            if a-c>=0:
+                dp[a] = min(dp[a], 1+ dp[a-c])
+    if dp[amount] != amount+1:
+        print(dp)
+        return dp[amount]
+
+if __name__=="__main__":
+    amount=11
+    coins = [1,2,5]
+    print(cc(coins,amount))
