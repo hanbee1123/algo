bdd15c3c83a66638328197d431164f276c57e691
DESKTOP-O459G30\hanbee <hanbee1123@gmail.com>
2023-08-19 21:31:15 +0900

    most_water

---
+++ DataStructures/store_water.py
@@ -0,0 +1,34 @@
+"""
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+"""
+
+class Solution:
+    def maxArea(self, height):
+        left = 0
+        right = len(height)-1
+        return_val = 0
+        while left != right:
+            area = min(height[left],height[right]) * (right - left)
+            print(area)
+            print(left,right)
+            return_val = max(return_val, area)
+            if height[left]<height[right]:
+                left+=1
+            else:
+                right -=1
+        return return_val
+
+if __name__ == "__main__":
+    h = [2,3,4,5,18,17,6]
+    s=Solution()
+    s.maxArea(h)
--- tester.py
+++ tester.py
@@ -1,37 +1,22 @@
-def intervals(intervals):
-    return_val = []
-    start = None
-    end = None
-    for i in range(len(intervals)-1):
+class Solution:
+    def longestPalindrome(self, s):
+        self.s = s
+        self.final_result = ''
 
-        if intervals[i][1] >= intervals[i+1][0]:
-            if start == None:
-                start = intervals[i][0]
-            else:
-                start = min(start,intervals[i][0])
-            continue
+        def check_ss(s):
+            return s == s[::-1]
 
-        elif  intervals[i][1] >= intervals[i+1][1]:
-            if start == None:
-                start = intervals[i][0]
-            else:
-                start = min(start,intervals[i][0])
 
-            if end == None:
-                end = intervals[i][1]
-            else:
-                end = max(end,intervals[i+1][0])
-            continue
+        def dfs(idx, substring):
+            if check_ss(substring) == True:
+                self.final_result = max(self.final_result, substring, key=len)
 
-        elif intervals[i][1] < intervals[i+1][0]:
-            end = intervals[i][1]
-        else:
-            return_val.append(intervals[i])
-
-        return_val.append([start,end])
-        start = None
-        end = None
-
+            if idx <= len(self.s)-1:
+                dfs(idx+1, substring+self.s[idx])
+            return
+        for i in range(len(s)):
+            dfs(i,'')
+        print(self.final_result)
 if __name__ == "__main__":
-    intervalss = [[1,3],[2,6],[8,10],[15,18]]
-    print(intervals(intervalss))
+    s = Solution()
+    s.longestPalindrome("babad")
